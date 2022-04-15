4e7502c805f60df8e6982a2ab9d43e8071504ec0
peterchu999 <peter.andrew987@gmail.com>
2022-04-15 09:29:57 +0700

    fix same generated prefix production material ui

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,16 +2,24 @@ import * as React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
           <MarketingApp />
         </div>
-      </BrowserRouter>
-    </>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
